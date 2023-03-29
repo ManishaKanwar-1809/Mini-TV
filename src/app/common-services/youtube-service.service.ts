799bb5746d8561d8c00e026fecf29dec0e8c2984
manisha_idcletech <mk18091998@gmail.com>
2023-03-29 13:41:35 +0530

    Add console log

--- src/app/common-services/youtube-service.service.ts
+++ src/app/common-services/youtube-service.service.ts
@@ -13,10 +13,10 @@ getVideos(channelName: any): Observable<any> {
     debugger
     const API_KEY = apiEnvironment.params.key;
     const url = `${apiEnvironment.baseURL}/search?part=${apiEnvironment.params.part}&q=${channelName}&type=${apiEnvironment.params.type}&key=${API_KEY}&maxResults=${apiEnvironment.params.maxResults}`
-
+console.log(url);
     // const url = apiEnvironment.baseURL+"/search?part="+apiEnvironment.part+"&q="+channelName+"&type=channel&key="+API_KEY+"&maxResults="+apiEnvironment.maxResults;
   return this.http.get<any>(url);
-  }
+  } 
 
   getChannels(channelName: any): Observable<any> {
     debugger
